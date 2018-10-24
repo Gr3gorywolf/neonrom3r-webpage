ccd378abe9811dea4059d202098d83bf64efebd0
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2018-10-24 01:07:54 -0400

    added new  post to the news section  :v

--- Data/Novedades.js
+++ Data/Novedades.js
@@ -6,6 +6,14 @@ data=[
 "texto":`En esta versión se incluyen todos los
 elementos básicos previamente descritos en la sección de funciones`,
 
-}
+},
+{
+    "titulo":"Caida de emulators.games :(",
+    "imagen":"imgs/v1-0.png",
+    "texto":`Lastimosamente emulators.games se ha protegido con cloudflare 
+    por lo cual es basicamente imposible obtener informacion de los roms y mucho menos descargarlos. 
+    Pero se esta pensando una forma para buscar roms de otra forma y hacer la app utilizable de nuevo`,
+    
+    }
 
 ]

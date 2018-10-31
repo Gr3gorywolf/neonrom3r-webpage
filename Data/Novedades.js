c49e7e8a889fb0d80665cdd0ebe8d396523ee7d6
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2018-10-31 00:05:53 -0400

    github pages

--- Data/Novedades.js
+++ Data/Novedades.js
@@ -8,7 +8,7 @@ elementos básicos previamente descritos en la sección de funciones`,
 
 },
 {
-    "titulo":"Caida de emulators.games :(",
+    "titulo":"Caida de emulators.games :,(",
     "imagen":"imgs/v1-0.png",
     "texto":`Lastimosamente emulators.games se ha protegido con cloudflare 
     por lo cual es basicamente imposible obtener informacion de los roms y mucho menos descargarlos. 

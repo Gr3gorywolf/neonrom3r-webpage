d3acec549dbe9e1c2de84af7b3b2539fdaa62a6e
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2018-11-01 12:38:40 -0400

    emulator.games returned!!

--- Data/Novedades.js
+++ Data/Novedades.js
@@ -8,12 +8,21 @@ elementos básicos previamente descritos en la sección de funciones`,
 
 },
 {
-    "titulo":"Caida de emulators.games :,(",
+    "titulo":"Caida de emulator.games :,(",
     "imagen":"imgs/v1-0.png",
     "texto":`Lastimosamente emulators.games se ha protegido con cloudflare 
     por lo cual es basicamente imposible obtener informacion de los roms y mucho menos descargarlos. 
     Pero se esta pensando una forma para buscar roms de otra forma y hacer la app utilizable de nuevo`,
     
-    }
+ }
+ ,
+{
+    "titulo":"El regreso de emulator.games",
+    "imagen":"imgs/v1-0.png",
+    "texto":`Es un día feliz puesto a que emulator.games volvió y ahora mas tiene mucha mejor
+     latencia por lo que ahora la aplicación obtiene información mas rápido`,
+    
+ }
+
 
 ]
